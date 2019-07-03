c9196aae1957e00bc40d2ad801870185af2d54e0
44742 <44742@ProgramminHelp>
2019-07-03 12:22:22 +0100

    commit vector example

---
+++ com.collection/src/net/set/VectorExample.java
@@ -0,0 +1,23 @@
+package net.set;
+
+import java.util.Vector;
+
+public class VectorExample {
+	public static void main(String[] args) {
+		/*
+		 * creating vector
+		 */
+		Vector<String> v = new Vector<String>();
+		/*
+		 * adding values to vector
+		 */
+		v.add("Mohammed");
+		v.add("Shafiq");
+		v.add("Fata");
+		/*
+		 * reading vector values using lambda for each
+		 */
+		v.forEach((value) -> System.out.println(value));
+
+	}
+}
